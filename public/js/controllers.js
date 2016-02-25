b65e3a8774831ab7e82fdc410eb7bfea30e19a4c
Chaz Leong <cleong14@gmail.com>
2016-02-24 21:47:47 -1000

    added factories

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,9 +3,11 @@ var myApp = angular.module('myApp');
 myApp.controller('MyController', [
   '$scope',
   'mainCharacter',
-  function($scope, mainCharacter) {
+  'CharacterVersionFactory',
+  function($scope, mainCharacter, CharacterVersionFactory) {
     $scope.myFirstName = 'Chaz';
     $scope.myModel = 'Ready Player One';
     $scope.mainCharacter = mainCharacter;
+    $scope.characterVersion = CharacterVersionFactory;
   }
 ]);

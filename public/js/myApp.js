ef37759427a3e48527e76b08c983bb9c6897b261
Chaz Leong <cleong14@gmail.com>
2016-02-24 20:42:30 -1000

    added angular module

---
+++ public/js/myApp.js
@@ -0,0 +1,13 @@
+angular.module('myApp', []);
+
+// good practice to always use myApp and use angular to go and fetch it
+var myApp = angular.module('myApp');
+
+myApp
+  .config(function () {
+    // set up configuration
+  })
+  .run(function () {
+    // start application
+    console.log('starting...');
+  });

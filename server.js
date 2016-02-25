adedce78c1ce243d70930a60ffbe9030d93f3848
Chaz Leong <cleong14@gmail.com>
2016-02-24 19:23:24 -1000

    Create server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var morgan = require ('morgan');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.resolve(__dirname + '/public')));
+
+app.use(morgan('dev'));
+
+var server = app.listen(3000, function() {
+  console.log('Listening to port', server.address().port);
+});

62d3b3474812de96da0fe1d742bf4f44b8a013fc
Chaz Leong <cleong14@gmail.com>
2016-02-25 21:56:29 -1000

    Separated files to make it cleaner

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -10,6 +10,18 @@ myApp
 
     // Routes
     $routeProvider
+      .when('/', {
+        templateUrl: '/templates/intro.html',
+        controller: 'MyController'
+      })
+      .when('/books', {
+        templateUrl: '/templates/books.html',
+        controller: 'MyController'
+      })
+      .when('/movies', {
+        templateUrl: '/templates/movies.html',
+        controller: 'MyController'
+      })
       .when('/404', {
         templateUrl: '/templates/404.html'
       })

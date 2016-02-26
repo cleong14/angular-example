04f31919371701109a70bedc8b13a80777408125
Chaz Leong <cleong14@gmail.com>
2016-02-25 21:38:52 -1000

    add 404 page

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,12 +1,19 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ngRoute']);
 
 // good practice to always use myApp and use angular to go and fetch it
 var myApp = angular.module('myApp');
 
 myApp
-  .config(function (MoviesProvider) {
+  .config(function ($routeProvider, MoviesProvider) {
     // set up configuration
     MoviesProvider.setEndpoint('/api');
+
+    // Routes
+    $routeProvider
+      .when('/404', {
+        templateUrl: '/templates/404.html'
+      })
+      .otherwise('/404');
   })
   .run([
     '$rootScope',

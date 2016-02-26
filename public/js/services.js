abb112cadc2e5528f0ba36af5c917ae752ce4eb6
Chaz Leong <cleong14@gmail.com>
2016-02-25 18:24:59 -1000

    refactored and made books a list of objects

--- public/js/services.js
+++ public/js/services.js
@@ -3,7 +3,12 @@ var myApp = angular.module('myApp');
 myApp.service('BookService', [BookService]);
 
 function BookService () {
-  var books = ['Harry Potter', 'The Hobbit', 'Javascript for Dummies', 'Coding for Dummies'];
+  var books = [
+  { title: 'Harry Potter', author: 'Some Body' },
+  { title: 'The Hobbit', author: 'Some Body' },
+  { title: 'Javascript for Dummies', author: 'Some Body' },
+  { title: 'Coding for Dummies', author: 'Some Body' }
+  ];
 
   this.getBooks = function () {
     return books;
